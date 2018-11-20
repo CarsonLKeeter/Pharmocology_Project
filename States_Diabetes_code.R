b97e7ff30a03af3e30b9df33eb0222bc1fe37350
Carson Keeter <44477356+CarsonLKeeter@users.noreply.github.com>
2018-11-20 14:21:25 -0700

    Update States_Diabetes_code.R

--- States_Diabetes_code.R
+++ States_Diabetes_code.R
@@ -2,55 +2,69 @@ df <- data %>%
   select(
     State, 
     Percentage
-    ) %>%
+  ) %>%
   filter(
     State == "Wyoming" | 
-    State == "Montana" | 
-    State == "Colorado"
-    )
+      State == "Montana" | 
+      State == "Colorado"
+  )
 
 ggplot(
   df, 
   aes(
     State, 
     Percentage
-    )
-  ) + 
+  )
+) + 
   geom_bar(
     stat = "identity", 
     data = df, 
     aes(
       x = State, 
       y = Percentage
-      ), 
+    ), 
     color = "Blue", 
     fill = "Blue"
-    ) +
+  ) +
   ggtitle(
     "Prevelance of Type II Diabetes in CO, MT, and WY"
-    ) +
+  ) +
   theme(
     axis.title.x = element_blank(
-    
+      
     ),
     axis.ticks.x = element_blank(
       
-    )
-  ) +
+    ), 
+    panel.grid.major = element_blank(
+      
+    ), 
+    panel.grid.minor = element_blank(
+      
+    ),
+    panel.background = element_blank(
+      
+    ),
+    axis.line = element_line(
+      color = "black"
+      )
+  ) + 
   ylim(
     0, 10
-    ) + 
+  ) + 
   geom_hline(
     aes(
       yintercept = 9.2
-      )
-    ) + 
+    ),
+    color = "grey", 
+    linetype = 2
+  ) + 
   geom_text(
     aes(
       1, 
       9.2, 
       label = "Median of US", 
       vjust = -1
-      )
     )
+  )
 
