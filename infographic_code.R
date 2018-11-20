e497c833e50bfdf68323a963138f315c617007a4
Carson Keeter <44477356+CarsonLKeeter@users.noreply.github.com>
2018-11-20 12:00:50 -0700

    Partial Infographic code

---
+++ infographic_code.R
@@ -0,0 +1,9 @@
+prevalence_plot <- waffle(
+  c(
+    "Diabetic (9.2%)" = 9, "Non-Diabetic (90.8%)" = 91
+    ), 
+  rows = 5, 
+  title = "Prevalence of Diabetes in the US",
+  legend_pos = "bottom"
+  )  
+prevalence_plot
